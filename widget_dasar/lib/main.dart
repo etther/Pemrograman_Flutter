eac1c5aafad98a2793ff700f3be6d6fc6265a547
Eter <famutashiro@gmail.com>
2023-10-10 01:28:58 +0700

    -

---
+++ widget_dasar/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("My Apps"),
+        ),
+        body: Center(
+          child: Text("HALO"),
+        ),
+      ),
+    );
+  }
+}
