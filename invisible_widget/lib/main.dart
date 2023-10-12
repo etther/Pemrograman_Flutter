1972196aac82380d94780e5a63084c1250364695
Eter <famutashiro@gmail.com>
2023-10-12 15:05:05 +0700

    list view

--- invisible_widget/lib/main.dart
+++ invisible_widget/lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
             Container(
               height: 100,
               width: 100,
-              color: Colors.yellowAccent,
+              color: Colors.yellow,
             )
           ],
         ),
